ef601b3909848fe6583ed2681d5d1208f78724eb
Stefanos Anagnostou <stefanos@clerk.dev>
2023-10-05 12:12:48 +0300

    feat(clerk-react,clerk-js): Add support for custom pages in OrganizationProfile

--- packages/clerk-js/src/ui/components/OrganizationProfile/OrganizationProfileRoutes.tsx
+++ packages/clerk-js/src/ui/components/OrganizationProfile/OrganizationProfileRoutes.tsx
@@ -1,6 +1,8 @@
+import { useOrganizationProfileContext } from '../../contexts';
 import { ProfileCardContent } from '../../elements';
 import { Route, Switch } from '../../router';
 import type { PropsOfComponent } from '../../styledSystem';
+import { ExternalElementMounter } from '../../utils';
 import { DeleteOrganizationPage, LeaveOrganizationPage } from './ActionConfirmationPage';
 import { AddDomainPage } from './AddDomainPage';
 import { InviteMembersPage } from './InviteMembersPage';
@@ -12,65 +14,87 @@ import { VerifiedDomainPage } from './VerifiedDomainPage';
 import { VerifyDomainPage } from './VerifyDomainPage';
 
 export const OrganizationProfileRoutes = (props: PropsOfComponent<typeof ProfileCardContent>) => {
+  const { pages } = useOrganizationProfileContext();
+  const isMembersPageRoot = pages.routes[0].id === 'members';
+  const isSettingsPageRoot = pages.routes[0].id === 'settings';
+  const isPredefinedPageRoot = isSettingsPageRoot || isMembersPageRoot;
+
   return (
     <ProfileCardContent contentRef={props.contentRef}>
-      <Route path='organization-settings'>
-        <Switch>
+      <Switch>
+        {/* Custom Pages */}
+        {pages.contents?.map((customPage, index) => (
           <Route
-            path='profile'
-            flowStart
+            index={!isPredefinedPageRoot && index === 0}
+            path={!isPredefinedPageRoot && index === 0 ? undefined : customPage.url}
+            key={`custom-page-${customPage.url}`}
           >
-            <ProfileSettingsPage />
+            <ExternalElementMounter
+              mount={customPage.mount}
+              unmount={customPage.unmount}
+            />
           </Route>
-          <Route
-            path='domain'
-            flowStart
-          >
+        ))}
+        <Route>
+          <Route path={isSettingsPageRoot ? undefined : 'organization-settings'}>
             <Switch>
-              <Route path=':id/verify'>
-                <VerifyDomainPage />
+              <Route
+                path='profile'
+                flowStart
+              >
+                <ProfileSettingsPage />
               </Route>
-              <Route path=':id/remove'>
-                <RemoveDomainPage />
+              <Route
+                path='domain'
+                flowStart
+              >
+                <Switch>
+                  <Route path=':id/verify'>
+                    <VerifyDomainPage />
+                  </Route>
+                  <Route path=':id/remove'>
+                    <RemoveDomainPage />
+                  </Route>
+                  <Route path=':id'>
+                    <VerifiedDomainPage />
+                  </Route>
+                  <Route index>
+                    <AddDomainPage />
+                  </Route>
+                </Switch>
               </Route>
-              <Route path=':id'>
-                <VerifiedDomainPage />
+              <Route
+                path='leave'
+                flowStart
+              >
+                <LeaveOrganizationPage />
+              </Route>
+              <Route
+                path='delete'
+                flowStart
+              >
+                <DeleteOrganizationPage />
               </Route>
               <Route index>
-                <AddDomainPage />
+                <OrganizationSettings />
               </Route>
             </Switch>
           </Route>
-          <Route
-            path='leave'
-            flowStart
-          >
-            <LeaveOrganizationPage />
+          <Route path={isMembersPageRoot ? undefined : 'organization-members'}>
+            <Switch>
+              <Route
+                path='invite-members'
+                flowStart
+              >
+                <InviteMembersPage />
+              </Route>
+              <Route index>
+                <OrganizationMembers />
+              </Route>
+            </Switch>
           </Route>
-          <Route
-            path='delete'
-            flowStart
-          >
-            <DeleteOrganizationPage />
-          </Route>
-          <Route index>
-            <OrganizationSettings />
-          </Route>
-        </Switch>
-      </Route>
-      <Route>
-        <Switch>
-          <Route
-            path='invite-members'
-            flowStart
-          >
-            <InviteMembersPage />
-          </Route>
-          <Route index>
-            <OrganizationMembers />
-          </Route>
-        </Switch>
-      </Route>
+        </Route>
+      </Switch>
     </ProfileCardContent>
   );
 };
--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -100,3 +100,6 @@ export type UserProfileLinkProps = {
   label: string;
   labelIcon: React.ReactElement;
 };
+
+export type OrganizationProfilePageProps = UserProfilePageProps;
+export type OrganizationProfileLinkProps = UserProfileLinkProps;
